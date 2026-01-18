206c3063163101a5084cb69a8b2ef2ef1487c68b
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-01-19 00:09:07 +0100

    aplicar layout de figma de Galeria

--- la-silla-2/src/components/Galeria/Galeria.jsx
+++ la-silla-2/src/components/Galeria/Galeria.jsx
@@ -17,13 +17,56 @@ const images = [
 export const Galeria = () => {
   const visibleImages = useGallerySwap(images, 3500, 2);
   return (
-    <div className="GaleriaSection">
-      <div className="GaleriaGrid">
-        {visibleImages.map((src, i) => (
-          <div key={i} className="Galeria-imgDiv">
-            <img src={src} alt="" className="Galeria-img" />
+    <div
+      style={{
+        width: "100%",
+        backgroundColor: "var(--bg-color)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div className="GaleriaSection">
+        <header className="GaleriaHeader">
+          <div className="GaleriaHeader-div">
+            <h3 className="GaleriaHeader-h3">NUESTROS CORTES</h3>
           </div>
-        ))}
+          <div className="GaleriaHeader-div GaleriaHeader-div--derecha ">
+            <h3 className="GaleriaHeader-h3">ESTILO PROPIO</h3>
+          </div>
+        </header>
+
+        <div className="GaleriaContent">
+          <div className="GaleriaIzq">
+            <img
+              src="/imgs/img-peluqueria.jpg"
+              alt=""
+              className="GaleriaIzq-img"
+            />
+            <div className="GaleriaContent-textoDiv">
+              <p className="GaleriaContent-texto">
+                EN LA SILLA 2 CUIDAMOS CADA CORTE COMO PARTE DE TU IDENTIDAD.
+              </p>
+              <p className="GaleriaContent-texto">
+                TRABAJO PRECISO, TRATO CERCANO Y UN ESPACIO PENSADO PARA
+                SENTIRTE CÓMODO.
+              </p>
+              <p className="GaleriaContent-texto">
+                UN ESPACIO CON ESTILO Y CRITERIO PARA ENCONTRAR EL CORTE QUE
+                MEJOR TE QUEDA.{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="GaleriaGrid">
+            {visibleImages.map((src, i) => (
+              <div key={i} className="Galeria-imgDiv">
+                <img src={src} alt="" className="Galeria-img" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <h2 className="Galeria-titulo">GALERÍA</h2>
       </div>
     </div>
   );
